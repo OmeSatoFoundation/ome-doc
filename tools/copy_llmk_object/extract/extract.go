864b51b507ea2e8fc31d4614c94e084b1800dff1
Yohei Shimmyo <qwpmb554@gmail.com>
2025-10-21 12:13:40 +0900

    add a utility to copy latex intermediate/object files to somewhere
    This commit merges below commits
    
    commit e225a971ded135c1aef62f37204cbdd1124a600a
    Author: Yohei Shimmyo <qwpmb554@gmail.com>
    Date:   Tue Oct 21 12:48:14 2025 +0900
    
        add a utility to copy latex intermediate/object files to somewhere
    
    commit 6714d47585dcdc7f7e2ece7863ff49d3a04a7b31
    Author: Yohei Shimmyo <qwpmb554@gmail.com>
    Date:   Tue Oct 21 12:33:31 2025 +0900
    
        escape percentages
    
    commit ee310f06b68112ab686618d1683ca7979f73a704
    Author: Yohei Shimmyo <qwpmb554@gmail.com>
    Date:   Tue Oct 21 12:30:29 2025 +0900
    
        compile passed
    
    commit 22c3b2170b4158e7300891d979fc3c4f869b8e07
    Author: Yohei Shimmyo <qwpmb554@gmail.com>
    Date:   Tue Oct 21 12:13:40 2025 +0900
    
        initialize a go project to copy latex object files

---
+++ tools/copy_llmk_object/extract/extract.go
@@ -0,0 +1,22 @@
+package extract
+
+import (
+	"github.com/BurntSushi/toml"
+	"os"
+	"path"
+)
+
+type config struct {
+	Source []string `toml:"source"`
+}
+
+func ExtractSourcesInLLMKToml(cwd string) ([]string, error) {
+	llmk_toml_path := path.Join(cwd, "llmk.toml")
+	tomlData, err := os.ReadFile(llmk_toml_path)
+	if err != nil {
+		return nil, err
+	}
+	var conf config
+	_, err = toml.Decode(string(tomlData), &conf)
+	return conf.Source, nil
+}
